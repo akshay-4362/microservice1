73b582126a8238186054f1dcc89c38bc5dc45454
Infect_dev <apple@infecteddev.local>
2024-10-07 03:19:16 +0530

    akshay

---
+++ app.js
@@ -0,0 +1,14 @@
+// Import Express module
+const express = require('express');
+const app = express();
+
+// Define a simple GET route
+app.get('/ms1', (req, res) => {
+  res.send('Hello, World from server 1');
+});
+
+// Set up the server to listen on port 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
