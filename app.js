eee78181796769e4977d9593f9e70c73cd3b43f8
Akshay Tulajannavar <66796540+akshay-4362@users.noreply.github.com>
2024-11-02 04:47:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,19 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-// app.use((req: Request, res: Response, next: NextFunction) => {
-//     res.setHeader('Connection', 'keep-alive');
-//     res.setHeader('Keep-Alive', 'timeout=30');
-//     next();
-// });
-// Define a simple GET route
-app.get('/ms1', (req, res) => {
-  res.json('Hello, World from server 1');
-});
 
-app.get('/test', (req, res) => {
-  res.json('TESTING THE DEPLOYMENT');
-});
 
 app.get('/data-from-ms2', async (req, res) => {
   try {
@@ -29,6 +17,16 @@ app.get('/data-from-ms2', async (req, res) => {
     res.status(500).json({ error: 'Error accessing Microservice 2' });
   }
 });
+
+app.get('/ms1/test', (req, res) => {
+  res.json('TESTING THE DEPLOYMENTs');
+});
+
+app.get('/ms1', (req, res) => {
+  res.json('Hello, World from server 1');
+});
+
+
 
 
 // Set up the server to listen on port 3000
