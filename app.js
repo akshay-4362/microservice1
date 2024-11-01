cb85715721d75368d20bdfe92ab546082fa219e8
Akshay Tulajannavar <66796540+akshay-4362@users.noreply.github.com>
2024-11-02 04:17:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Import Express module
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
 // app.use((req: Request, res: Response, next: NextFunction) => {
@@ -11,6 +12,20 @@ const app = express();
 app.get('/ms1', (req, res) => {
   res.json('Hello, World from server 1');
 });
+
+app.get('/data-from-ms2', async (req, res) => {
+  try {
+    // Make a GET request to Microservice 2 running on port 3001
+    const response = await axios.get('http://two-serive-cloud-map:3001/ms2');
+    res.json({
+      message: 'Data from Microservice 2',
+      data: response.data,
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Error accessing Microservice 2' });
+  }
+});
+
 
 // Set up the server to listen on port 3000
 const PORT = 3000;
