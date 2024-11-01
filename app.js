0dbb50da99ffb2d656c4ed23dbef171c5fb4e71b
Akshay Tulajannavar <66796540+akshay-4362@users.noreply.github.com>
2024-11-01 19:22:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 const express = require('express');
 const app = express();
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-    res.setHeader('Connection', 'keep-alive');
-    res.setHeader('Keep-Alive', 'timeout=30');
-    next();
-});
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//     res.setHeader('Connection', 'keep-alive');
+//     res.setHeader('Keep-Alive', 'timeout=30');
+//     next();
+// });
 // Define a simple GET route
 app.get('/ms1', (req, res) => {
   res.json('Hello, World from server 1');
@@ -18,5 +18,5 @@ const server =  app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-server.keepAliveTimeout = 30 * 1000; // <- important lines
-server.headersTimeout = 35 * 1000;
+// server.keepAliveTimeout = 30 * 1000; // <- important lines
+// server.headersTimeout = 35 * 1000;
